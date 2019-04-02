cdaa56c21541fd0abf3957385e5955e411eee6f1
Matt Freeland <matthewjfreeland@gmail.com>
2019-04-01 19:21:39 -0700

    adds models

--- app/__init__.py
+++ app/__init__.py
@@ -1,15 +1,8 @@
 from flask import Flask, url_for
-# from flask_assets import Environment, Bundle
-# from flask_sqlalchemy import SQLAlchemy
-
+from app.models import User, Band, Venue, FavBand, Rating, Friend, Event
 
 app = Flask(__name__)
 app.static_folder = 'static'
-# assets = Environment(app)
 app.config['SECRET_KEY'] = "beepboopiamrobotbeepboop"
-# scss = Bundle('main.scss', filters='pyscss', output='styles.css')
-# assets.register('scss_all',scss)
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://localhost/server'
-# db = SQLAlchemy(app)
 
 from app import routes
---
+++ app/models.py
@@ -0,0 +1,210 @@
+from peewee import *
+import datetime, time, moment
+
+from flask_login import UserMixin
+from flask_bcrypt import generate_password_hash
+
+DATABASE = SqliteDatabase('intothepit.db')
+
+#--------------# PRIMARY MODELS #--------------#
+
+class User(UserMixin, Model):
+    username = CharField(unique=True)
+    email = CharField(unique=True, max_length=256)
+    password = CharField(max_length=128)
+    avatar = BlobField(default="default.png")
+    city = CharField()
+    state = CharField()
+    zip = CharField()
+
+    class Meta:
+        database = DATABASE
+
+    @classmethod
+    def create_user(cls, username, email, password, city, state, zip, avatar="default.png"):
+        try:
+            cls.create(
+                username=username,
+                email=email,
+                password=generate_password_hash(password),
+                avatar="default.png",
+                city=city,
+                state=state,
+                zip=zip
+            )
+        except IntegrityError:
+            raise ValueError("User already exists")
+
+class Band(Model):
+    bandname = CharField()
+    about = CharField()
+    genre = CharField()
+    themes = CharField()
+    img = BlobField(default="default_band.png")
+    bigimg = BlobField(default="default_band_bg.png")
+
+    class Meta:
+        database = DATABASE
+
+    @classmethod
+    def create_band(cls, bandname, about, genre, themes, img="default_band.png", bigimg="default_band_bg.png"):
+        try:
+            cls.create(
+                bandname=bandname,
+                about=about,
+                genre=genre,
+                themes=themes,
+                img="default_band.png",
+                bigimg="default_band_bg.png"
+            )
+        except IntegrityError:
+            raise
+
+class Venue(Model):
+    name = CharField()
+    img = BlobField(default="default_venue.png")
+    about = CharField()
+    address = CharField()
+    city = CharField()
+    state = CharField()
+    zip = CharField()
+
+    class Meta:
+        database = DATABASE
+
+    @classmethod
+    def create_venue(cls, name, about, address, city, state, zip, img="default_venue.png"):
+        try:
+            cls.create(
+                name=name,
+                img="default_venue.png",
+                about=about,
+                address=address,
+                city=city,
+                state=state,
+                zip=zip
+            )
+        except IntegrityError:
+            raise
+
+class Rating(Model):
+    user_fk = ForeignKeyField(
+        model=User,
+        backref="users"
+    )
+    venue_fk = ForeignKeyField(
+        model=Venue,
+        backref="venues",
+        null=True
+    )
+    band_fk = ForeignKeyField(
+        model=User,
+        backref="bands",
+        null=True
+    )
+    rating = IntegerField()
+    message = CharField(max_length=1024)
+
+    class Meta:
+        database = DATABASE
+
+    @classmethod
+    def create_rating(cls,user_fk, venue_fk, band_fk, rating, message):
+        try:
+            cls.create(
+                user_fk=user_fk,
+                venue_fk=venue_fk,
+                band_fk=band_fk,
+                rating=rating,
+                message=message,
+            )
+        except IntegrityError:
+            raise
+
+class Event(Model):
+    band_fk = ForeignKeyField(
+        model=User,
+        backref="bands",
+        null=True
+    )
+    venue_fk = ForeignKeyField(
+        model=Venue,
+        backref="venues",
+        null=True
+    )
+    date = DateTimeField()
+
+    class Meta:
+        database = DATABASE
+
+    @classmethod
+    def create_event(cls,band_fk,venue_fk,date):
+        try:
+            cls.create(
+                band_fk=band_fk,
+                venue_fk=venue_fk,
+                date=date
+            )
+        except IntegrityError:
+            raise
+
+class Friend(Model):
+    friender = ForeignKeyField(
+        model=User,
+        backref="friends"
+    )
+    friendee = ForeignKeyField(
+        model=User,
+        backref="friends",
+        null=True
+    )
+
+    class Meta:
+        database = DATABASE
+
+    @classmethod
+    def create_friend(cls,friender,friendee):
+        try:
+            cls.create(
+                friender=friender,
+                friendee=friendee
+            )
+        except IntegrityError:
+            raise
+
+class FavBand(Model):
+    user_fk = ForeignKeyField(
+        model=User,
+        backref="users"
+    )
+    band_fk = ForeignKeyField(
+        model=User,
+        backref="bands",
+        null=True
+    )
+
+    class Meta:
+        database = DATABASE
+
+    @classmethod
+    def create_favband(cls,user_fk,band_fk):
+        try:
+            cls.create(
+                user_fk=user_fk,
+                band_fk=band_fk
+            )
+        except IntegrityError:
+            raise
+
+
+def initialize():
+    DATABASE.connect()
+    DATABASE.create_tables([User, Band, Venue, Event, Friend, FavBand, Rating], safe=True)
+    DATABASE.close()
+
+
+
+
+
+
+
