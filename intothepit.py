7cec74e2bafe25cafd81b794146f59c40fabfdf9
Walrus Maximus <matthewjfreeland@gmail.com>
2019-05-13 01:18:20 -0700

    testing initalizer, is that crashing?

--- intothepit.py
+++ intothepit.py
@@ -2,6 +2,10 @@ from app import app
 from app import models
 import os
 
+if 'ON_HEROKU' in os.environ:
+    models.initialize()
+
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 8000))
+    models.initialize()
     app.run(debug=True, port=port)
