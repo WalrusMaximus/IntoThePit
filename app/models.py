d3ceb030d21837eae7b71babd8d98f845b9c9b1c
Walrus Maximus <matthewjfreeland@gmail.com>
2019-05-27 15:25:44 -0700

    changed routepath to routes

--- app/models.py
+++ app/models.py
@@ -6,10 +6,10 @@ from flask_login import UserMixin
 from flask_bcrypt import generate_password_hash
 from playhouse.db_url import connect
 
-# DATABASE = SqliteDatabase('intothepit.db')
+DATABASE = SqliteDatabase('intothepit.db')
 
 # DATABASE = PostgresqlDatabase('intothepit')
-DATABASE = connect(os.environ.get('DATABASE_URL'))
+# DATABASE = connect(os.environ.get('DATABASE_URL'))
 
 #--------------# PRIMARY MODELS #--------------#
 
