ab1347d804ff636f34abc3ab7d6e95fcd632b357
walrusmaximus <matthewjfreeland@gmail.com>
2019-06-21 14:39:23 -0700

    corrects env variables to deploy to production env

--- intothepit.py
+++ intothepit.py
@@ -3,12 +3,10 @@ from peewee import *
 from app import app, models
 
 if __name__ == '__main__':
-    if os.environ.get('HEROKU_PRODUCTION'):
+    if os.environ.get('IS_HEROKU'):
         print("Launching in Production Environment")
-        PRODUCTION = os.environ.get('PRODUCTION') or True
         app.run(debug=False)
     else:
         models.initialize()
         print("Launching in Development Environment - Debug Active")
-        PRODUCTION = os.environ.get('PRODUCTION') or False
         app.run(host='0.0.0.0', debug=True, port=8000)
