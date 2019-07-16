4eef2c02c1f632bcbe3e7983015202b2cce15e46
walrusmaximus <matthewjfreeland@gmail.com>
2019-07-16 09:50:30 -0700

    AHHHHHHHHHHHHH

--- intothepit.py
+++ intothepit.py
@@ -3,11 +3,11 @@ from peewee import *
 from app import app, models
 
 if __name__ == '__main__':
-    if os.environ.get('IS_HEROKU'):
-        print("Launching in Production Environment")
-        models.initialize()
-        app.run(debug=False)
-    else:
-        models.initialize()
-        print("Launching in Development Environment - Debug Active")
-        app.run(host='0.0.0.0', debug=True, port=8000)
+    # if os.environ.get('IS_HEROKU'):
+    print("Launching in Production Environment")
+    models.initialize()
+    app.run(debug=False)
+    # else:
+    #     models.initialize()
+    #     print("Launching in Development Environment - Debug Active")
+    #     app.run(host='0.0.0.0', debug=True, port=8000)
